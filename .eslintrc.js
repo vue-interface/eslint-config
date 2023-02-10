57d716a572f3359535da3cea1bfaafefa22574db
Mitchell Solomon <msolomon@actengage.com>
2023-02-10 14:20:30 -0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    extends: [
+        'plugin:vue/vue3-recommended',
+        '@vue/eslint-config-typescript'
+    ],
+    rules: {
+        'vue/component-tags-order': ['error', {
+            'order': ['script', 'template', 'style']
+        }],
+        'vue/component-name-in-template-casing': ['warn', 'PascalCase'],
+        'quotes': [2, 'single', { 'avoidEscape': true }],
+        'vue/multi-word-component-names': 0,
+        'vue/html-indent': ['error', 4, {
+            'attribute': 1,
+            'baseIndent': 1,
+            'closeBracket': 1,
+            'alignAttributesVertically': false,
+            'ignores': []
+        }],
+        'vue/html-closing-bracket-newline': ['error', {
+            'singleline': 'never',
+            'multiline': 'never'
+        }],
+        'keyword-spacing': ['error', {
+            'before': true,
+            'after': true,
+            'overrides': {
+                'if': { 'after': false },
+                'while': { 'after': false },
+                'for': { 'after': false },
+            }
+        }],
+        'indent': ['error', 4],
+        'semi': [2, 'always'],
+        'object-curly-spacing': ['error', 'always'],
+        'space-before-blocks': ['error', 'always']
+    }
+};
